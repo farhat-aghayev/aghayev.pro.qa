cb5214ca48874d1ff1badc3683f0915c6ef15b14
farhat-aghayev <farhat.aghayev@outlook.com>
2024-03-26 02:25:01 +0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,7 @@ function customRehypeLazyLoadImage() {
 }
 
 export default defineConfig({
-  site: 'https://jakobnutson.github.io',
-  base: '/astro-yi',
+  site: 'https://aghayev.pro',
   integrations: [sitemap(), tailwind(), solid(), expressiveCode({
     plugins: [pluginLineNumbers()],
     themes: ["github-dark", "github-light"],
